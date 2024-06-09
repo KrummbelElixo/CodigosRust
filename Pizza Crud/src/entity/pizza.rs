218e1666fcf4d6a5777ea6ea56fc898388113321
KrummbelElixo <47263386+KrummbelElixo@users.noreply.github.com>
2024-06-09 00:22:26 -0300

    Add files via upload

---
+++ Pizza Crud/src/entity/pizza.rs
@@ -0,0 +1,55 @@
+
+use core::fmt;
+use crate::entity::RuleSet;
+pub struct Pizza {
+    nome: String,
+    tipo: TipoPizza,
+    valor: f32,
+}
+
+impl Pizza {
+    pub fn new(nome: String, tipo: TipoPizza, valor: f32) -> Self {
+        Pizza {
+            nome,
+            tipo,
+            valor,
+        }
+    }
+
+    pub fn set_nome(&mut self) -> &mut String {
+        &mut self.nome
+    }
+
+    pub fn set_tipo(&mut self) -> &mut TipoPizza {
+        &mut self.tipo
+    }
+
+    pub fn set_valor(&mut self) -> &mut f32 {
+        &mut self.valor
+    }
+}
+
+impl RuleSet for Pizza {
+    fn print_struct(&self) {
+        println!("Pizza: {} Tipo: {} Valor: {}", self.nome, self.tipo, self.valor);
+    }
+}
+
+impl fmt::Display for Pizza {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Pizza: {}\n   Tipo: {}\n   Valor: R${}", self.nome, self.tipo, self.valor)
+    }
+}
+
+#[derive(Debug)]
+pub enum TipoPizza {
+    Salgada,
+    Doce,
+    NaoEspecificado
+}
+
+impl fmt::Display for TipoPizza {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
