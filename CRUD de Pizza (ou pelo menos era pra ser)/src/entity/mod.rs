b0527f91f43e84dc937afff116324a975b672251
KrummbelElixo <47263386+KrummbelElixo@users.noreply.github.com>
2024-06-09 00:21:04 -0300

    Delete CRUD de Pizza (ou pelo menos era pra ser) directory

--- CRUD de Pizza (ou pelo menos era pra ser)/src/entity/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod pizza;
-pub mod user;
-
-pub trait RuleSet {
-    fn print_struct(&self);
-}
--- CRUD de Pizza (ou pelo menos era pra ser)/target/debug/deps/pizza_crud.d
+++
@@ -1,8 +0,0 @@
-C:\Users\sprin\Downloads\myrust\pizza_crud\target\debug\deps\pizza_crud.exe: src\main.rs src\entity\mod.rs src\entity\pizza.rs src\entity\user.rs
-
-C:\Users\sprin\Downloads\myrust\pizza_crud\target\debug\deps\pizza_crud.d: src\main.rs src\entity\mod.rs src\entity\pizza.rs src\entity\user.rs
-
-src\main.rs:
-src\entity\mod.rs:
-src\entity\pizza.rs:
-src\entity\user.rs:
